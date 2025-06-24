5d8b16fe4b38606b9a9600392894231eac1dbb22
Robert Huang <56649783@qq.com>
2025-06-25 00:20:05 +0800

    ci: ⚙️update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/bash
+
 ps aux | grep docs-server-vertx.jar | head -n 1 | awk '{print $2}' | xargs kill -9
 ps aux | grep docs-server-vertx-fat.jar | head -n 1 | awk '{print $2}' | xargs kill -9
