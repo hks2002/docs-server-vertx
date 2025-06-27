d895cb8b89fd1ac3c6f45b53fd6809f87292846d
Robert Huang <56649783@qq.com>
2025-06-27 17:25:22 +0800

    chore: ♻️update shell script

--- startup-fat.sh
+++ startup-fat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 ./shutdown-fat.sh
-java -jar docs-server-vertx-fat.jar --conf=config-prod.json --options=vertx-options.json &
+java -jar docs-server-vertx-fat.jar --conf=config-prod.json --options=vertx-options.json
 
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 ./shutdown.sh
-java -jar docs-server-vertx.jar --conf=config-prod.json --options=vertx-options.json &
+java -jar docs-server-vertx.jar --conf=config-prod.json --options=vertx-options.json
