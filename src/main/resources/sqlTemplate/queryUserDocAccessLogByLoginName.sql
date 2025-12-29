f45bbca86f7a81b18f0decbbb712846dd9790428
Robert Huang <56649783@qq.com>
2025-12-29 15:55:46 +0800

    feat: ✨better history sql

--- src/main/resources/sqlTemplate/queryUserDocAccessLogByLoginName.sql
+++ src/main/resources/sqlTemplate/queryUserDocAccessLogByLoginName.sql
@@ -1,12 +1,14 @@
 WITH T0 AS (
-	SELECT 
-	  v3 AS file_name,
-	  log_at AS access_time,
-	  ROW_NUMBER() OVER (PARTITION BY v3, DATE(log_at) ORDER BY log_at DESC) AS rn
-	FROM log
-	WHERE log.template_id = 9 
-	AND v1 = '#{login_name}'
-	ORDER BY id DESC
+  SELECT
+    v3 AS file_name,
+    log_at AS access_time,
+    ROW_NUMBER() OVER (PARTITION BY v3, DATE(log_at) ORDER BY log_at DESC) AS rn
+  FROM log
+  INNER JOIN log_template
+    ON log.template_id = log_template.id
+    AND log_template.template_code = 'DOC_ACCESS_SUCCESS'
+  AND v1 = '#{login_name}'
+  ORDER BY log.id DESC
 )
 
 SELECT
@@ -16,7 +18,7 @@ SELECT
 FROM T0
 LEFT JOIN docs
    ON T0.file_name = docs.file_name
-WHERE 
+WHERE
    rn = 1
 LIMIT
   #{limit}
